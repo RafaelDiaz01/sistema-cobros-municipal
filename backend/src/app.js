a19f290821ebcc37684a2d66b682b27c84db1081
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2025-11-12 17:16:16 -0600

    Agregar asociaciones para las relaciones de las tablas y rutas de establecimientos

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
-import contribuyenteRoutes from "./routes/contribuyenteRoutes.js";
-import usuariosRoutes from "./routes/usuarioRoutes.js";
+import contribuyenteRoutes from "./routes/contribuyenteRoute.js";
+import usuariosRoutes from "./routes/usuarioRoute.js";
+import establecimientoRoutes from "./routes/establecimientoRoute.js";
+
+// Cargar modelos y asociaciones para que estén disponibles al arrancar
+import "./models/associations.js";
 
 const app = express();
 
@@ -9,6 +13,7 @@ app.use(express.json());
 
 // Rutas
 app.use("/api/contribuyentes", contribuyenteRoutes);
-app.use("/api/usuarios", usuariosRoutes)
+app.use("/api/usuarios", usuariosRoutes);
+app.use("/api/establecimientos", establecimientoRoutes);
 
 export default app;

c26e1b99babb61e837f817db088426d0478607e8
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2025-11-14 16:25:20 -0600

    Agregar ruta de bases catastrales

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import contribuyenteRoutes from "./routes/contribuyenteRoute.js";
 import usuariosRoutes from "./routes/usuarioRoute.js";
 import establecimientoRoutes from "./routes/establecimientoRoute.js";
+import baseCatastralRoutes from "./routes/baseCatastralRoute.js";
 
 // Cargar modelos y asociaciones para que estén disponibles al arrancar
 import "./models/associations.js";
@@ -15,5 +16,6 @@ app.use(express.json());
 app.use("/api/contribuyentes", contribuyenteRoutes);
 app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/establecimientos", establecimientoRoutes);
+app.use("/api/bases-catastrales", baseCatastralRoutes);
 
 export default app;

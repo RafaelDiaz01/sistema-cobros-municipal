dbdc217dd05e4a4123745c47a2782756da404402
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2026-01-15 11:13:34 -0600

    feat: agregar ruta por defecto, ruta 404, y rutas para dos páginas

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,13 +1,19 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/Login";
 import Contribuyentes from "../pages/Contribuyentes";
+import Mas from "../pages/Mas";
+import Establecimientos from "../pages/Establecimientos";
 
 const AppRouter = () => {
   return (
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/contribuyentes" element={<Contribuyentes />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/contribuyentes" element={<Contribuyentes />} />
+      <Route path="/mas" element={<Mas />} />
+      <Route path="/establecimientos" element={<Establecimientos />} />
+      <Route path="*" element={<h1>Página no encontrada</h1>} />
+    </Routes>
   );
 };
 

ed0fb466e583a8ed9ad3f803119100db28323aa5
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2026-01-16 11:06:20 -0600

    feat: agregar PrivateRoute para proteger las rutas privadas en el FrontEnd

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+
 import Login from "../pages/Login";
 import Contribuyentes from "../pages/Contribuyentes";
 import Mas from "../pages/Mas";
@@ -7,12 +9,17 @@ import Establecimientos from "../pages/Establecimientos";
 const AppRouter = () => {
   return (
     <Routes>
+      {/* Rutas Publicas */}
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/contribuyentes" element={<Contribuyentes />} />
-      <Route path="/mas" element={<Mas />} />
-      <Route path="/establecimientos" element={<Establecimientos />} />
       <Route path="*" element={<h1>Página no encontrada</h1>} />
+
+      {/* Rutas Privadas */}
+      <Route element={<PrivateRoute />}>
+        <Route path="/contribuyentes" element={<Contribuyentes />} />
+        <Route path="/mas" element={<Mas />} />
+        <Route path="/establecimientos" element={<Establecimientos />} />
+      </Route>
     </Routes>
   );
 };

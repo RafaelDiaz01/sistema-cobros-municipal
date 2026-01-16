54503154fdc90a3e84bf68e533a2e07213c69374
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-16 16:18:44 -0600

    feat: agregar authMiddleware para proteger las rutas

--- backend/src/routes/contribuyenteRoute.js
+++ backend/src/routes/contribuyenteRoute.js
@@ -1,18 +1,19 @@
 import express from "express";
 import * as contribuyenteController from "../controllers/contribuyenteController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 // Obtener todos los contribuyentes
-router.get("/", contribuyenteController.getContribuyentes);
+router.get("/", authMiddleware, contribuyenteController.getContribuyentes);
 
 // Crear un nuevo contribuyente
-router.post("/", contribuyenteController.postContribuyente);
+router.post("/", authMiddleware, contribuyenteController.postContribuyente);
 
 // Actualizar el estado de un contribuyente
-router.put("/:id/estado", contribuyenteController.putContribuyenteEstado);
+router.put("/:id/estado", authMiddleware, contribuyenteController.putContribuyenteEstado);
 
 // Actualizar datos de un contribuyente
-router.put("/:id" , contribuyenteController.putContribuyente);
+router.put("/:id", authMiddleware, contribuyenteController.putContribuyente);
 
 export default router;

940abce5a447cb1f854f2d3977a4a62f8b526cc9
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-19 17:52:53 -0600

    feat: agregar función para consumir la api de crear y de actualizar un establecimiento

--- frontend/src/api/establecimientos.js
+++ frontend/src/api/establecimientos.js
@@ -1,5 +1,11 @@
 import api from "./axios.js";
 
+// Crear un nuevo establecimiento
+export const createEstablecimientoAPI = async (data) => {
+  const response = await api.post("/establecimientos", data);
+  return response.data;
+};
+
 // Obtener todos los establecimientos
 export const getEstablecimientos = async () => {
   const response = await api.get("/establecimientos");
@@ -9,5 +15,11 @@ export const getEstablecimientos = async () => {
 // Actualizar el estado (activo/inactivo) de un establecimiento
 export const updateStatusEstablecimientoAPI = async (id, data) => {
   const response = await api.put(`/establecimientos/${id}/estado`, data);
+  return response.data;
+};
+
+// Actualizar los datos de un establecimiento
+export const updateEstablecimientoAPI = async (id, data) => {
+  const response = await api.put(`/establecimientos/${id}`, data);
   return response.data;
 };

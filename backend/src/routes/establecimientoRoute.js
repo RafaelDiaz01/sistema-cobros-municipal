6fe492d5dfc2b54de9223ac83d3c7d661cbf8782
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-19 16:34:38 -0600

    feat:  agregar rutas para actualizar el estado y los datos de un establecimiento

--- backend/src/routes/establecimientoRoute.js
+++ backend/src/routes/establecimientoRoute.js
@@ -1,15 +1,19 @@
 import express from "express";
-import {
-  crearEstablecimiento,
-  obtenerEstablecimientos,
-} from "../controllers/establecimientoController.js";
+import * as establecimientoController from "../controllers/establecimientoController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 // Obtener todos los establecimientos
-router.get("/", obtenerEstablecimientos);
+router.get("/", authMiddleware, establecimientoController.obtenerEstablecimientos);
 
 // Crear un nuevo establecimiento
-router.post("/", crearEstablecimiento);
+router.post("/", authMiddleware, establecimientoController.crearEstablecimiento);
+
+// Actualizar el estado de un establecimiento
+router.put("/:id/estado", authMiddleware, establecimientoController.putEstablecimientoEstado);
+
+// Actualizar datos de un establecimiento
+router.put("/:id", authMiddleware, establecimientoController.putEstablecimiento);
 
 export default router;

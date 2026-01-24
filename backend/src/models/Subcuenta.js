9e59396d293f25395aaa92749ab5b7af87cf2e6e
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-24 14:20:08 -0600

    feat: agregar un valor por defecto al atributo monto_base

--- backend/src/models/Subcuenta.js
+++ backend/src/models/Subcuenta.js
@@ -23,6 +23,7 @@ const Subcuenta = sequelize.define("subcuenta", {
   monto_base: {
     type: DataTypes.DECIMAL(12, 2),
     allowNull: false,
+    defaultValue: 0.00,
   },
   activo: {
     type: DataTypes.BOOLEAN,

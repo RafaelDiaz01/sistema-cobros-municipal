85be1f5c8aac6b937ee0c29d6d86bc6d9aac0e39
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-25 14:38:16 -0600

    feat: modificar componente para mostrar datos desde el backend

--- frontend/src/pages/cobrar/ContribuyenteCard.jsx
+++ frontend/src/pages/cobrar/ContribuyenteCard.jsx
@@ -1,22 +1,65 @@
-import { User } from "lucide-react";
 import CardCobro from "../../components/cards/CardCobro";
+import Stack from "../../components/layouts/Stack.jsx";
+import Grid from "../../components/modals/components/Grid.jsx";
 
-export default function ContribuyenteCard() {
+export default function ContribuyenteCard({ contribuyente }) {
   return (
-    <CardCobro className="flex flex-col gap-3">
-      <div className="flex items-center gap-2 text-sm font-semibold">
-        <User size={16} className="text-green-600" />
-        Contribuyente Seleccionado
-      </div>
-
-      <div className="text-sm">
-        <p className="font-medium">Juan Carlos Pérez</p>
-        <p className="text-xs text-gray-500">RFC: XTS190722DEF</p>
-        <p className="text-xs text-gray-500">Clave Catastral: 02-005-0012</p>
-        <p className="text-xs text-gray-500 mt-1">
-          Av. Juárez 123, Centro, Ixtlán de Juárez, Oax.
-        </p>
-      </div>
+    <CardCobro title="Contribuyente Seleccionado">
+      <Grid cols={3}>
+        <div className="col-span-2">
+          <ItemContribuyente
+            label="Nombre"
+            value={
+              [
+                contribuyente?.nombre,
+                contribuyente?.apellido_paterno,
+                contribuyente?.apellido_materno,
+              ]
+                .filter(Boolean)
+                .join(" ") || "No Seleccionado"
+            }
+          />
+        </div>
+        <div className="col-span-1">
+          <ItemContribuyente
+            label="Clave Única"
+            value={
+              [contribuyente?.id_contribuyente].filter(Boolean).join(" ") ||
+              "No Seleccionado"
+            }
+          />
+        </div>
+        <div className="col-span-2">
+          <ItemContribuyente
+            label="Dirección"
+            value={
+              [contribuyente?.barrio].filter(Boolean).join(" ") ||
+              "No Seleccionado"
+            }
+          />
+        </div>
+        <div className="col-span-1">
+          <ItemContribuyente
+            label="RFC"
+            value={
+              [contribuyente?.rfc].filter(Boolean).join(" ") ||
+              "No Seleccionado"
+            }
+          />
+        </div>
+      </Grid>
     </CardCobro>
+  );
+}
+
+// Componente para mostrar los datos del contribuyente seleccionado
+function ItemContribuyente({ label, value }) {
+  return (
+    <Stack size="xs">
+      <p className="text-sm text-gray-500">{label}</p>
+      <div className="border border-dashed border-gray-300 rounded-md p-3 bg-gray-50 flex items-center gap-2">
+        <p className="text-sm font-semibold">{value}</p>
+      </div>
+    </Stack>
   );
 }

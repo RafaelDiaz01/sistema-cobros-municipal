0d78a5c0d4db3940d3bc9a1fba59ac9aa622e3c1
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-25 14:33:52 -0600

    style: cambiar el grosor de las etiquetas p

--- frontend/src/components/cobros/InfoItem.jsx
+++ frontend/src/components/cobros/InfoItem.jsx
@@ -1,8 +1,8 @@
 export default function InfoItem({ label, value }) {
   return (
     <div className="flex items-center justify-between">
-      <p className="text-sm font-bold">{label}</p>
-      <p className="text-sm font-medium">{value}</p>
+      <p className="text-sm font-medium">{label}</p>
+      <p className="text-sm font-bold">{value}</p>
     </div>
   );
 }

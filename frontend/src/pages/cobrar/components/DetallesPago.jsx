0dbd4f6449af714465d7b1b78623f6f02d9fadd9
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-25 14:46:51 -0600

    feat: crear componente DetallesPago

---
+++ frontend/src/pages/cobrar/components/DetallesPago.jsx
@@ -0,0 +1,43 @@
+import CardCobro from "../../../components/cards/CardCobro.jsx";
+import Grid from "../../../components/modals/components/Grid.jsx";
+import Input from "../../../components/modals/components/Input.jsx";
+import Select from "../../../components/modals/components/Select.jsx";
+import TextArea from "../../../components/modals/components/TextArea.jsx";
+import BuscarConceptoPago from "../../../components/cobros/BuscarConceptoPago.jsx";
+
+export default function DetallesPago({
+  onSelectConcepto,
+  searchConceptoPagoAPI,
+}) {
+  return (
+    <CardCobro title="Detalles del Pago">
+      <label className="text-sm font-medium">
+        {"Concepto de Pago"} <span className="text-red-500">*</span>
+      </label>
+      <BuscarConceptoPago
+        onSelect={onSelectConcepto}
+        searchFn={searchConceptoPagoAPI}
+      />
+      <Grid cols={4}>
+        <Input label="Monto a Pagar" placeholder="Ej. 1,250.00" />
+        <Input
+          label="Periodo de Pago"
+          placeholder="Ej. Enero - Diciembre 2024"
+        />
+        <Input label="Descuento Regular" placeholder="Ej. 50%" />
+        <Select
+          label="Método de Pago"
+          options={["Efectivo", "Transferencia Bancaria"]}
+        />
+      </Grid>
+      <p class="text-[11px] text-gray-400 italic">
+        Nota: El descuento adicional por pronto pago solo aplica en los meses de
+        enero y febrero.
+      </p>
+      <TextArea
+        label="Descripción"
+        placeholder="Ej. Pago de Predial correspondiente al año 2024"
+      />
+    </CardCobro>
+  );
+}

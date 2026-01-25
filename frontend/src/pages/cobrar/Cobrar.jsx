8f8e2d2f5a6e44841f9128db1c58a2aed75a15de
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-25 14:47:30 -0600

    feat: modificar la page dividiendola en componentes

--- frontend/src/pages/cobrar/Cobrar.jsx
+++ frontend/src/pages/cobrar/Cobrar.jsx
@@ -1,17 +1,14 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { searchConceptoPagoAPI } from "../../api/conceptoPago.js";
 import PageLayout from "../../components/layouts/PageLayout";
 import Stack from "../../components/layouts/Stack";
 import SectionTitleSimple from "../../components/titles/SectionTitleSimple";
 import CardCobro from "../../components/cards/CardCobro";
-import Grid from "../../components/modals/components/Grid";
 import BuscarContribuyente from "./BuscarContribuyente";
 import ResumenRecibo from "./ResumenRecibo";
-import Input from "../../components/modals/components/Input.jsx";
-import TextArea from "../../components/modals/components/TextArea.jsx";
-import InfoItem from "../../components/cobros/InfoItem.jsx";
-import BuscarConceptoPago from "../../components/cobros/BuscarConceptoPago.jsx";
-import { searchConceptoPagoAPI } from "../../api/conceptoPago.js";
+import ContribuyenteCard from "./ContribuyenteCard.jsx";
+import DetallesPago from "./components/DetallesPago.jsx";
 
 export default function Cobrar() {
   const [contribuyente, setContribuyente] = useState(null);
@@ -28,90 +25,22 @@ export default function Cobrar() {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           {/* IZQUIERDA */}
           <div className="lg:col-span-2 flex flex-col gap-6">
-            <CardCobro title="Buscar Contribuyente">
-              <BuscarContribuyente onSelect={setContribuyente} />
-            </CardCobro>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <CardCobro title="Buscar Contribuyente">
+                <BuscarContribuyente onSelect={setContribuyente} />
+              </CardCobro>
+              <ContribuyenteCard contribuyente={contribuyente} />
+            </div>
 
-            <CardCobro title="Detalles del Pago">
-              <BuscarConceptoPago
-                onSelect={handleSelectConcepto}
-                searchFn={searchConceptoPagoAPI}
-              />
-              <Grid cols={3}>
-                <Input
-                  // {...register("nombre", { required: true })}
-                  label="Monto a Pagar"
-                  placeholder="Ej. 1,250.00"
-                />
-                <Input
-                  // {...register("nombre", { required: true })}
-                  label="Periodo de Pago"
-                  placeholder="Ej. Enero - Diciembre 2024"
-                />
-                <Input label="Descuento Regular" placeholder="Ej. 50%" />
-              </Grid>
-              <p class="mt-1.5 text-[11px] text-gray-500 dark:text-gray-400 italic">
-                Nota: El descuento adicional por pronto pago solo aplica en los
-                meses de enero y febrero.
-              </p>
-              <TextArea
-                label="Descripción"
-                placeholder="Ej. Pago de Predial correspondiente al año 2024"
-              />
-            </CardCobro>
+            <DetallesPago
+              onSelectConcepto={handleSelectConcepto}
+              searchConceptoPagoAPI={searchConceptoPagoAPI}
+            />
           </div>
 
           {/* DERECHA */}
           <div className="flex flex-col gap-6">
-            <CardCobro title="Contribuyente Seleccionado">
-              <Grid cols={2}>
-                <div>
-                  <InfoItem
-                    label="Nombre Completo"
-                    value={
-                      [
-                        contribuyente?.nombre,
-                        contribuyente?.apellido_paterno,
-                        contribuyente?.apellido_materno,
-                      ]
-                        .filter(Boolean)
-                        .join(" ") || "No seleccionado"
-                    }
-                  />
-                </div>
-                <div>
-                  <InfoItem
-                    label="RFC"
-                    value={
-                      [contribuyente?.rfc].filter(Boolean).join(" ") ||
-                      "No seleccionado"
-                    }
-                  />
-                </div>
-                <div>
-                  <InfoItem
-                    label="Clave Catastral"
-                    value={
-                      [contribuyente?.id_contribuyente]
-                        .filter(Boolean)
-                        .join(" ") || "No seleccionado"
-                    }
-                  />
-                </div>
-                <div>
-                  <InfoItem
-                    label="Dirección"
-                    value={
-                      [contribuyente?.barrio].filter(Boolean).join(" ") ||
-                      "No seleccionado"
-                    }
-                  />
-                </div>
-              </Grid>
-            </CardCobro>
-
-            {/* RESUMEN DEL RECIBO */}
-            <ResumenRecibo concepto={conceptoSeleccionado} />
+            <ResumenRecibo concepto={conceptoSeleccionado} contribuyente={contribuyente} />
           </div>
         </div>
       </Stack>

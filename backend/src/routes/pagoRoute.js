ea408bb64f709abbd63f6ac20c66eefe01bb5081
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-26 17:44:44 -0600

    feat: agregar authMiddleware para la seguridad del backend

--- backend/src/routes/pagoRoute.js
+++ backend/src/routes/pagoRoute.js
@@ -1,21 +1,22 @@
 import { Router } from "express";
 import PagoController from "../controllers/pagoController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 /*
   POST /api/pagos
 */
-router.post("/", PagoController.registrar);
+router.post("/", authMiddleware, PagoController.registrar);
 
 /*
   GET /api/pagos
 */
-router.get("/", PagoController.listar);
+router.get("/", authMiddleware, PagoController.listar);
 
 /*
   GET /api/pagos/:id
 */
-router.get("/:id", PagoController.obtenerPorId);
+router.get("/:id", authMiddleware, PagoController.obtenerPorId);
 
 export default router;

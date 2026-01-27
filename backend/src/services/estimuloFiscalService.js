deb7d7d83b4e35cb8b808fc555ba0db2af8b5264
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2026-01-27 16:00:54 -0600

    feat: modificar para crear una clase y sus funciones static

--- backend/src/services/estimuloFiscalService.js
+++ backend/src/services/estimuloFiscalService.js
@@ -2,27 +2,31 @@ import EstimuloFiscal from "../models/EstimuloFiscal.js";
 import { Op } from "sequelize";
 
 // Buscar estímulos fiscales por nombre o descripción
-export const buscarEstimulosFiscales = async (texto) => {
-  return await EstimuloFiscal.findAll({
-    where: {
-      [Op.or]: [
-        {
-          nombre: {
-            [Op.like]: `%${texto}%`,
+class estimuloFiscalService {
+  static async buscarEstimulosFiscales(texto) {
+    return await EstimuloFiscal.findAll({
+      where: {
+        [Op.or]: [
+          {
+            nombre: {
+              [Op.like]: `%${texto}%`,
+            },
           },
-        },
-        {
-          descripcion: {
-            [Op.like]: `%${texto}%`,
+          {
+            descripcion: {
+              [Op.like]: `%${texto}%`,
+            },
           },
-        },
-      ],
-    },
-    limit: 10,
-  });
-};
+        ],
+      },
+      limit: 10,
+    });
+  }
 
-// Obtener todos los estímulos fiscales
-export const obtenerTodosEstimulosFiscales = async () => {
-  return await EstimuloFiscal.findAll();
-};
+  // Obtener todos los estímulos fiscales
+  static async obtenerTodos() {
+    return await EstimuloFiscal.findAll();
+  }
+}
+
+export default estimuloFiscalService;

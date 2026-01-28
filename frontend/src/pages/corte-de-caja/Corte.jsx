eec4c3d6dfb797ef96df1f5d786aa858a016ec60
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2026-01-27 22:26:59 -0600

    feat: crear page para el corte de caja

---
+++ frontend/src/pages/corte-de-caja/Corte.jsx
@@ -0,0 +1,13 @@
+import PageLayout from "../../components/layouts/PageLayout.jsx";
+import Stack from "../../components/layouts/Stack.jsx";
+import SectionSimpleTitle from "../../components/titles/SectionTitleSimple.jsx";
+
+export default function Corte() {
+    return (
+        <PageLayout>
+            <Stack size="xl">
+                <SectionSimpleTitle text="Corte de Caja" />
+            </Stack>
+        </PageLayout>
+    );
+};

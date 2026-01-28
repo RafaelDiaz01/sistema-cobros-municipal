20a2adf4804141686f5b30834b442a7649e7fc2e
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2026-01-27 23:26:35 -0600

    feat: agregar componente para mostrar la información

--- frontend/src/pages/corte-de-caja/Corte.jsx
+++ frontend/src/pages/corte-de-caja/Corte.jsx
@@ -1,12 +1,45 @@
+import {
+    Landmark,
+    Clock,
+    Calendar,
+} from "lucide-react";
 import PageLayout from "../../components/layouts/PageLayout.jsx";
 import Stack from "../../components/layouts/Stack.jsx";
+import Grid from "../../components/modals/components/Grid.jsx";
 import SectionSimpleTitle from "../../components/titles/SectionTitleSimple.jsx";
+import InfoBadge from "./components/InfoBadge.jsx";
+import StatsCards from "../../components/cards/StatsCards.jsx";
 
 export default function Corte() {
     return (
         <PageLayout>
             <Stack size="xl">
-                <SectionSimpleTitle text="Corte de Caja" />
+                <Grid cols={2} className="items-center">
+                    <SectionSimpleTitle text="Corte de Caja" />
+                    <Grid cols={4}>
+                        <InfoBadge
+                            label="Estado: Abierto"
+                            status
+                        />
+
+                        <InfoBadge
+                            icon={Landmark}
+                            label="Caja: Principal"
+                        />
+
+                        <InfoBadge
+                            icon={Clock}
+                            label="Turno: Matutino"
+                        />
+
+                        <InfoBadge
+                            icon={Calendar}
+                            label="27/01/2026"
+                        />
+                    </Grid>
+                </Grid>
+
+                <StatsCards />
             </Stack>
         </PageLayout>
     );

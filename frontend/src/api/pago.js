c218c2bb38c840b69789c0116c09ed795e331749
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-28 22:47:07 -0600

    feat: agregar función para consumir la API de obtener todos los pagos

--- frontend/src/api/pago.js
+++ frontend/src/api/pago.js
@@ -5,3 +5,9 @@ export const createPagoAPI = async (data) => {
   const response = await api.post("/pagos", data);
   return response.data;
 };
+
+// Obtener todos los pagos
+export const getPagosAPI = async () => {
+  const response = await api.get("/pagos");
+  return response.data;
+};

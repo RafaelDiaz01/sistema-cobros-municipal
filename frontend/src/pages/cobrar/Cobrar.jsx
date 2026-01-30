9404bef35aa90881f137316b6bf1c5b7586cfb0e
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-30 15:03:56 -0600

    feat: agregar función para iniciar turno y useEffect para saber si hay corte activo

--- frontend/src/pages/cobrar/Cobrar.jsx
+++ frontend/src/pages/cobrar/Cobrar.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { showToast } from "../../utils/alerts/toast.js";
 import { searchConceptoPagoAPI } from "../../api/conceptoPago.js";
 import { searchEstimuloFiscalAPI } from "../../api/estimulosFiscales.js";
+import { getCorteActivoAPI } from "../../api/corteCaja.js";
+import { iniciarCorteCajaAPI } from "../../api/corteCaja.js";
 import PageLayout from "../../components/layouts/PageLayout";
 import Stack from "../../components/layouts/Stack";
-import SectionTitleSimple from "../../components/titles/SectionTitleSimple";
+import SectionTitleCobrar from "./components/SectionTitleCobrar.jsx";
 import CardCobro from "../../components/cards/CardCobro";
 import BuscarContribuyente from "./components/BuscarContribuyente.jsx";
 import ResumenRecibo from "./components/ResumenRecibo.jsx";
@@ -15,6 +18,7 @@ export default function Cobrar() {
   const [contribuyente, setContribuyente] = useState(null);
   const [conceptoSeleccionado, setConceptoSeleccionado] = useState(null);
   const [estimuloSeleccionado, setEstimuloSeleccionado] = useState(null);
+  const [corteActivo, setCorteActivo] = useState(null);
 
   // React Hook Form
   const form = useForm({
@@ -36,6 +40,22 @@ export default function Cobrar() {
     }
   }, [contribuyente, form]);
 
+  useEffect(() => {
+    const loadCorte = async () => {
+      try {
+        const userStr = localStorage.getItem("user");
+        const user = userStr ? JSON.parse(userStr) : null;
+        const id_usuario = user ? user.id : null;
+        const data = await getCorteActivoAPI(id_usuario);
+        setCorteActivo(data);
+      } catch (error) {
+        console.error("Error al cargar los datos del corte activo", error);
+      }
+    };
+
+    loadCorte();
+  }, []);
+
   const handleSelectConcepto = (concepto) => {
     setConceptoSeleccionado(concepto);
     form.setValue("tipo_referencia", concepto?.tipo);
@@ -46,12 +66,30 @@ export default function Cobrar() {
 
   const handleSelectEstimulo = (estimulo) => {
     setEstimuloSeleccionado(estimulo);
-  }
+  };
+
+  const handleIniciarTurno = async () => {
+    try {
+      const userStr = localStorage.getItem("user");
+      const user = userStr ? JSON.parse(userStr) : null;
+      const id_usuario = user ? user.id : null;
+      const corte = await iniciarCorteCajaAPI(id_usuario, 0);
+      setCorteActivo(corte);
+
+      showToast("success", "Caja Iniciada");
+    } catch (error) {
+      alert("Error al iniciar turno");
+    }
+  };
 
   return (
     <PageLayout>
       <Stack size="xl">
-        <SectionTitleSimple text="Procesar Cobros Municipales" />
+        <SectionTitleCobrar
+          text="Procesar Cobros Municipales"
+          corteActivo={corteActivo}
+          onAdd={handleIniciarTurno}
+        />
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           {/* IZQUIERDA */}
           <div className="lg:col-span-2 flex flex-col gap-6">

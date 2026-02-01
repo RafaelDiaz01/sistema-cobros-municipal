c09f642223945932c8094125821e4edb2c6e4255
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-31 21:37:34 -0600

    style: modificar el color del texto dependiendo el value

--- frontend/src/pages/cobrar/components/InfoItem.jsx
+++ frontend/src/pages/cobrar/components/InfoItem.jsx
@@ -1,8 +1,9 @@
 export default function InfoItem({ label, value }) {
+  const isNotSelected = value === "No Seleccionado";
   return (
     <div className="flex items-center justify-between">
       <p className="text-sm font-medium">{label}</p>
-      <p className="text-sm font-bold">{value}</p>
+      <p className={`text-sm font-medium ${isNotSelected ? "text-gray-400" : "text-gray-800"}`}>{value}</p>
     </div>
   );
 }

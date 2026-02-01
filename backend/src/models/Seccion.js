734bd7b898fef689f88ae9a99a29908411e34947
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-02-01 16:10:42 -0600

    feat: agregar freezeTableName y agregar defaultValue a monto_base

--- backend/src/models/Seccion.js
+++ backend/src/models/Seccion.js
@@ -1,33 +1,40 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/database.js";
 
-const Seccion = sequelize.define("seccion", {
-  id_seccion: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Seccion = sequelize.define(
+  "seccion",
+  {
+    id_seccion: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    clave_seccion: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+      unique: true,
+    },
+    nombre: {
+      type: DataTypes.STRING(150),
+      allowNull: false,
+    },
+    es_cobrable: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    monto_base: {
+      type: DataTypes.DECIMAL(12, 2),
+      allowNull: false,
+      defaultValue: 0.0,
+    },
+    activo: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
   },
-  clave_seccion: {
-    type: DataTypes.STRING(15),
-    allowNull: false,
-    unique: true,
+  {
+    freezeTableName: true, // Evita que Sequelize pluralice el nombre de la tabla
   },
-  nombre: {
-    type: DataTypes.STRING(150),
-    allowNull: false,
-  },
-  es_cobrable: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-  monto_base: {
-    type: DataTypes.DECIMAL(12, 2),
-    allowNull: true,
-  },
-  activo: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true,
-  },
-});
+);
 
 export default Seccion;

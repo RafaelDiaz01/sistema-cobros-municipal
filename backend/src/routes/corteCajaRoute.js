f86c69d3bf009b6acbb6b684559c1242de171d1f
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-02-02 20:11:08 -0600

    feat: agregar authMiddleware a las rutas

--- backend/src/routes/corteCajaRoute.js
+++ backend/src/routes/corteCajaRoute.js
@@ -1,18 +1,27 @@
 import { Router } from "express";
 import CorteCajaController from "../controllers/corteCajaController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 // Crear un nuevo corte de caja
-router.post("/abrir", CorteCajaController.abrir);
+router.post("/abrir", authMiddleware, CorteCajaController.abrir);
 
 // Cerrar un corte de caja existente
-router.put("/cerrar/:id", CorteCajaController.cerrar);
+router.put("/cerrar/:id", authMiddleware, CorteCajaController.cerrar);
 
 // Listar cortes de caja por usuario
-router.get("/usuario/:id_usuario", CorteCajaController.listarPorUsuario);
+router.get(
+  "/usuario/:id_usuario",
+  authMiddleware,
+  CorteCajaController.listarPorUsuario,
+);
 
 // Obtener datos del corte activo de un usuario
-router.get("/activo/:id_usuario", CorteCajaController.obtenerCorteActivo);
+router.get(
+  "/activo/:id_usuario",
+  authMiddleware,
+  CorteCajaController.obtenerCorteActivo,
+);
 
 export default router;

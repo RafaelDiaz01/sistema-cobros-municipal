92f4f440df41cc1820b9a2467f7ab59df38ff29d
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-02-02 20:00:04 -0600

    feat: agregar atributo id_usuario como FK

--- backend/src/models/Pago.js
+++ backend/src/models/Pago.js
@@ -18,6 +18,11 @@ const Pago = sequelize.define("pago", {
     allowNull: false,
   },
 
+  id_usuario: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+
   tipo_referencia: {
     type: DataTypes.STRING(20),
     allowNull: false,

6cfb0bbd923a4e437a2a0d6d2dede2bd0af55d0f
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-02-07 22:22:27 -0600

    feat: modificar ruta del endpoint

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import api from "./axios";
 
 export const loginAPI = async (credentials) => {
-  const response = await api.post("/usuarios/login", credentials);
+  const response = await api.post("/auth/login", credentials);
   return response.data;
 };

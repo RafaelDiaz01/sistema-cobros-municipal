2ad26a5086e3554f28c73063ec4834c6f5b9bf79
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-02-07 22:25:37 -0600

    feat: agregar AuthProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
+import { AuthProvider } from "./context/authContext.jsx";
 import theme from "./theme/theme";
-import '../src/assets/styles/global.css';
+import "../src/assets/styles/global.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );

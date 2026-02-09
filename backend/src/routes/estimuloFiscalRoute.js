a5cf1eb733946a4b29b99fd3747fe204b5b8d4a2
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-02-09 14:24:15 -0600

    feat: agregar ruta de actualizar esto y Middlewares

--- backend/src/routes/estimuloFiscalRoute.js
+++ backend/src/routes/estimuloFiscalRoute.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import estimuloFiscalController from "../controllers/estimuloFiscalController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 // Obtener todos los estímulos fiscales
-router.get("/", estimuloFiscalController.obtenerTodos);
+router.get("/", authMiddleware, estimuloFiscalController.obtenerTodos);
+
+// Actualizar el estado del estímulo fiscal
+router.patch("/:id/estado", authMiddleware, estimuloFiscalController.actualizarEstado);
 
 // Buscar estímulos fiscales por texto. Ejemplo: GET /api/estimulos/buscar?texto=Descuento
-router.get("/buscar", estimuloFiscalController.buscar);
+router.get("/buscar", authMiddleware, estimuloFiscalController.buscar);
+
 
 export default router;

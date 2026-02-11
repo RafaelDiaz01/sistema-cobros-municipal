e505f41b89f37cd1c52d577dd7627a97d7122f57
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-02-10 18:05:46 -0600

    feat: agregar lógica para mostrar modal, editar, y desactivar un subconcepto

--- frontend/src/pages/gestion-subconceptos/GestionSubconceptos.jsx
+++ frontend/src/pages/gestion-subconceptos/GestionSubconceptos.jsx
@@ -1,15 +1,21 @@
 import { useState, useEffect, useMemo } from "react";
 import { FileText, Check, X, HandCoins } from "lucide-react";
 import { getSubconceptosAPI } from "../../services/subconceptoService.js";
+import { updateSubconceptoEstadoAPI } from "../../services/subconceptoService.js";
+import { showToast } from "../../utils/alerts/toast.js";
+import { alertConfirmation } from "../../utils/alerts/alert.js";
 import { subconceptosColumns } from "./subconceptos.columns.jsx";
 import PageLayout from "../../components/layouts/PageLayout.jsx";
 import Stack from "../../components/layouts/Stack.jsx";
 import SectionTittle from "../../components/titles/SectionTitle.jsx";
 import StatsCards from "../../components/cards/StatsCards.jsx";
 import Table from "../../components/table/Table.jsx";
+import AddSubconceptoModal from "../../components/features/subconceptos/AddSubconceptosModal.jsx";
 
 export default function Subconceptos() {
   const [subconceptos, setSubconceptos] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [subconceptosEdit, setSubconceptosEdit] = useState(null);
 
   useEffect(() => {
     fetchSubconceptos();
@@ -49,12 +55,38 @@ export default function Subconceptos() {
     ];
   }, [subconceptos]);
 
-  const handleDelete = (id) => {
-    // Lógica para eliminar la sección
+  // Abrir modal para crear
+  const handleAdd = () => {
+    setSubconceptosEdit(null);
+    setOpen(true);
   };
 
-  const handleEdit = (id) => {
-    // Lógica para editar la sección
+  const handleDelete = async (id, estadoActual) => {
+    const nuevoEstado = !estadoActual;
+    const mensaje = nuevoEstado
+      ? "¿Deseas activar este subconcepto?"
+      : "¿Deseas desactivar este subconcepto?";
+
+    const confirmacion = await alertConfirmation(
+      "Atención",
+      mensaje,
+      "warning",
+    );
+    if (!confirmacion) return;
+
+    try {
+      await updateSubconceptoEstadoAPI(id, { estado: nuevoEstado });
+      fetchSubconceptos();
+      showToast("success", "Estado actualizado exitosamente");
+    } catch (error) {
+      console.error("Error al cambiar el estado subconcepto", error);
+      showToast("error", "Error al cambiar el estado del subconcepto");
+    }
+  };
+
+  const handleEdit = (subconcepto) => {
+    setSubconceptosEdit(subconcepto);
+    setOpen(true);
   };
 
   return (
@@ -62,8 +94,17 @@ export default function Subconceptos() {
       <Stack>
         <SectionTittle
           text="Gestión de Subconceptos"
+          onAdd={handleAdd}
           textButton="Agregar Subconcepto"
         />
+        {open && (
+          <AddSubconceptoModal
+            isOpen={open}
+            onClose={() => setOpen(false)}
+            subconcepto={subconceptosEdit}
+            onSuccess={fetchSubconceptos}
+          />
+        )}
         <StatsCards stats={stats} />
         <Table
           rows={subconceptos}

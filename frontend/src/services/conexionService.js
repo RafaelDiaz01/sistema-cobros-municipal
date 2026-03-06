4d1b8146f1d6f49762aa533def7717658c6d806b
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-03-06 10:44:18 -0600

    feat: crear archivo para consumir las APIS de conexion

---
+++ frontend/src/services/conexionService.js
@@ -0,0 +1,25 @@
+import api from "../api/axios.js";
+
+// Obtener todas los conexiones
+export const getConexionesAPI = async () => {
+  const response = await api.get("/conexiones");
+  return response.data;
+};
+
+// Crear una conexión
+export const createConexionAPI = async (data) => {
+  const response = await api.post("/conexiones", data);
+  return response.data;
+};
+
+// Actualizar una conexión
+export const updateConexionAPI = async (id, data) => {
+  const response = await api.patch(`/conexiones/${id}`, data);
+  return response.data;
+};
+
+// Actualizar estado de una conexión
+export const updateConexionEstadoAPI = async (id, estado) => {
+  const response = await api.patch(`/conexiones/${id}/estado`, estado);
+  return response.data;
+};

6ba4c987a79d60045ec350b30dc699b2162fb2de
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-04-21 15:47:07 -0600

    feat: mejorar estilos y estructura del componente Navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,34 +2,29 @@ import { NavLink } from "react-router-dom";
 import { UserCircle, Landmark, Settings } from "lucide-react";
 import ProfileDropdown from "./ProfileDropdown.jsx";
 
-const linkClass = ({ isActive }) =>
-  `hover:text-[var(--color-acento)] transition ${
-    isActive ? "text-[var(--color-acento)] font-semibold" : "text-black"
-  }`;
-
 export default function Navbar() {
   return (
-    <header className="w-full bg-[var(--color-fondo)] px-8 py-6">
+    <header className="w-full px-8 py-6">
       {/* CONTENIDO */}
       <div className="flex items-center justify-between pb-2">
         {/* ICONO Y NOMBRE DEL SISTEMA */}
-        <div className="flex items-center gap-3">
-          <Landmark size={22} className="text-[var(--color-primario)]" />
+        <div className="flex items-center gap-4">
+          <Landmark size={24} className="text-[var(--color-primario)]" />
           <h1 className="text-lg font-semibold">Sistema de Cobros Municipal</h1>
         </div>
 
         {/* OPCIONES DE NAVEGACIÓN */}
-        <nav className="flex items-center gap-8 text-sm">
+        <nav className="flex items-center gap-16 text-sm font-medium">
           <NavLink to="/cobrar" className={linkClass}>
             Cobrar
           </NavLink>
 
-          <NavLink to="/contribuyentes" className={linkClass}>
-            Contribuyentes
-          </NavLink>
-
           <NavLink to="/corte-caja" className={linkClass}>
             Corte de Caja
+          </NavLink>
+
+          <NavLink to="/contribuyentes" className={linkClass}>
+            Contribuyentes
           </NavLink>
 
           <NavLink to="/mas" className={linkClass}>
@@ -48,3 +43,10 @@ export default function Navbar() {
     </header>
   );
 }
+
+// Clase para los enlaces de navegación
+const linkClass = ({ isActive }) =>
+  `px-4 py-1 rounded-xl transition-all duration-200
+   ${isActive
+    ? "bg-[var(--color-primario)] text-white shadow-sm"
+    : "text-[var(--color-texto)] hover:bg-[var(--color-acento)] hover:text-white"}`;

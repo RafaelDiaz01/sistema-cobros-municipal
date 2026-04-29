51581100042d772c76bd89fcdd5d9a10c2c97199
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-04-29 17:18:07 -0600

    feat: crear componente GlobalFooter

---
+++ frontend/src/pages/login/components/GlobalFooter.jsx
@@ -0,0 +1,10 @@
+export default function GlobalFooter() {
+    return (
+        <div className="absolute bottom-4 w-full text-center text-xs text-black/80">
+            ¿Necesita ayuda?{" "}
+            <span className="font-semibold underline cursor-pointer">
+                Soporte Técnico
+            </span>
+        </div>
+    );
+}

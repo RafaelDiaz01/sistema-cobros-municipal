acc85ea92c5d99e9064b3b6c8689c9bb709d8cc9
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-04-30 10:07:18 -0600

    feat: agregar funcionalidad para mandar un mensaje por whatsapp

--- frontend/src/pages/login/components/GlobalFooter.jsx
+++ frontend/src/pages/login/components/GlobalFooter.jsx
@@ -1,10 +1,20 @@
 export default function GlobalFooter() {
+    const phone = "529515801224";
+    const message = "Hola, necesito soporte técnico con el sistema de cobros municipales.";
+
+    const whatsappUrl = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
     return (
-        <div className="absolute bottom-4 w-full text-center text-xs text-black/80">
+        <div className="absolute bottom-4 w-full text-center text-xs text-black/80 z-20 pointer-events-auto">
             ¿Necesita ayuda?{" "}
-            <span className="font-semibold underline cursor-pointer">
+            <a
+                href={whatsappUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-semibold underline cursor-pointer"
+            >
                 Soporte Técnico
-            </span>
+            </a>
         </div>
     );
 }

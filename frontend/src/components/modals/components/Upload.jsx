fe38e29c1c94e85a4c38ab30dee5851521ea447c
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-05-05 09:53:47 -0600

    feat: agregar lógica para mostrar error en el componente

--- frontend/src/components/modals/components/Upload.jsx
+++ frontend/src/components/modals/components/Upload.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { ImageUp } from "lucide-react";
 
-function Upload({ name, field, defaultImage }) {
+function Upload({ name, field, defaultImage, error }) {
   const [preview, setPreview] = useState(defaultImage || null);
 
   // Este useEffect limpia el objeto URL cuando el componente se desmonte o cuando cambie la imagen
@@ -20,33 +20,37 @@ function Upload({ name, field, defaultImage }) {
   };
 
   return (
-    <label
-      htmlFor={name}
-      className="border-2 border-dashed rounded-xl p-8 text-center text-sm text-gray-500 block cursor-pointer flex flex-col items-center gap-3 hover:border-[var(--color-primario)] transition-colors"
-    >
-      {preview ? (
-        <><img
-          src={preview}
-          alt="Preview"
-          className="w-32 h-32 object-cover rounded-full"
+    <>
+      <label
+        htmlFor={name}
+        className={`border-2 border-dashed rounded-xl p-8 text-center text-sm block cursor-pointer flex flex-col items-center gap-3
+          ${error ? "border-red-500 text-red-500" : "text-gray-500 hover:border-[var(--color-primario)]"}`}
+      >
+        {preview && !error ? (
+          <>
+            <img
+              src={preview}
+              alt="Preview"
+              className="w-32 h-32 object-cover rounded-full"
+            />
+            <p>Si deseas cambiar la imagen, haz clic aquí</p>
+          </>
+        ) : (
+          <>
+            <ImageUp className={error ? "text-red-500" : "text-gray-400"} />
+            <p>{error ? error : "Haz clic para subir una imagen"}</p>
+          </>
+        )}
+        <input
+          id={name}
+          type="file"
+          accept="image/*"
+          className="sr-only"
+          {...field}
+          onChange={handleFileChange}
         />
-          <p>Si deseas cambiar la imagen, haz clic aquí</p>
-        </>
-      ) : (
-        <>
-          <ImageUp className="text-gray-400" />
-          <p>Selecciona una imagen de tu dispositivo</p>
-        </>
-      )}
-      <input
-        id={name}
-        type="file"
-        accept="image/*"
-        className="sr-only"
-        {...field}
-        onChange={handleFileChange}
-      />
-    </label>
+      </label>
+    </>
   );
 }
 

a76bc2fb231f061e08e4e4a43bc84ecb620e7fe6
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-05-06 11:47:15 -0600

    feat: agregar regla de validación de correo al schema de usuario

--- frontend/src/validations/rules/index.js
+++ frontend/src/validations/rules/index.js
@@ -1,5 +1,6 @@
 // Exportar reglas de validación para formularios
 export * from "./string.rules.js";
 export * from "./phone.rules.js";
+export * from "./email.rules.js";
 export * from "./password.rules.js";
 export * from "./file.rules.js";
--- frontend/src/validations/schemas/user.schema.js
+++ frontend/src/validations/schemas/user.schema.js
@@ -1,10 +1,11 @@
 // Esquema de validación para el formulario de creación/edición de usuario utilizando Yup
 import * as yup from "yup";
-import { nameRule, phoneRule, fotoPerfilRule, passwordRequired, passwordStrong, confirmPassword, passwordNotSame } from "../rules";
+import { nameRule, phoneRule, emailRule, fotoPerfilRule, passwordRequired, passwordStrong, confirmPassword, passwordNotSame } from "../rules";
 
 export const createUserSchema = yup.object({
   nombre_completo: nameRule,
   telefono: phoneRule,
+  correo: emailRule,
   foto_perfil: fotoPerfilRule,
 });
 

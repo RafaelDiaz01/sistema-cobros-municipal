41c7a3ba8ece9d510bc8f66669aff0582e7aa1ec
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-05-13 12:53:38 -0600

    feat: modificar regla de validación para RFC

--- frontend/src/validations/rules/rfc.rules.js
+++ frontend/src/validations/rules/rfc.rules.js
@@ -2,10 +2,14 @@ import * as yup from "yup";
 
 export const rfcRule = yup
     .string()
-    .uppercase()
-    .matches(
-        /^([A-ZÑ&]{3,4})\d{6}([A-Z\d]{3})$/,
-        "El RFC no tiene un formato válido"
-    )
-    .min(12, "El RFC debe tener 12 o 13 caracteres")
-    .max(13, "El RFC debe tener 12 o 13 caracteres");
+    .nullable()
+    .transform((value) => (value === "" ? null : value))
+    .test(
+        "rfc-format",
+        "El RFC no tiene un formato válido",
+        (value) => {
+            if (!value) return true;
+
+            return /^([A-ZÑ&]{3,4})\d{6}([A-Z\d]{3})$/.test(value);
+        }
+    );

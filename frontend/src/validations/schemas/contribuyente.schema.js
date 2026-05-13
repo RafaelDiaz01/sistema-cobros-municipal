a8220fdfb4af0a0a3a4752ef4ba9b78defe27e87
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-05-13 10:48:49 -0600

    feat: eliminar validación de correo electrónico en contribuyentes

--- frontend/src/validations/schemas/contribuyente.schema.js
+++ frontend/src/validations/schemas/contribuyente.schema.js
@@ -1,13 +1,12 @@
 // Esquema de validación para el formulario de contribuyente utilizando Yup
 import * as yup from "yup";
-import { nameRule, lastNameRule, phoneRule, emailRule, rfcRule, birthdateRule } from "../rules";
+import { nameRule, lastNameRule, phoneRule, rfcRule, birthdateRule } from "../rules";
 
 export const createContribuyenteSchema = yup.object({
     nombre: nameRule,
     apellido_paterno: lastNameRule,
     apellido_materno: lastNameRule,
     telefono: phoneRule,
-    correo: emailRule,
     rfc: rfcRule,
     fecha_nacimiento: birthdateRule,
 });

59dac7cec762f00d4d932f5ab26415d863fb78bb
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-05-14 15:53:40 -0600

    style: modificar clase del botón para agregar

--- frontend/src/components/titles/SectionTitle.jsx
+++ frontend/src/components/titles/SectionTitle.jsx
@@ -11,7 +11,7 @@ export default function SectionTitle({ text, onAdd, textButton }) {
       {/* BOTÓN */}
       <button
         onClick={onAdd}
-        className="flex items-center gap-2 bg-[var(--color-primario)] text-white font-medium px-5 py-2.5 rounded-xl hover:opacity-90 transition"
+        className="flex items-center gap-2 bg-[var(--color-primario)] text-white font-medium px-4 py-2 rounded-xl"
       >
         {textButton}
       </button>

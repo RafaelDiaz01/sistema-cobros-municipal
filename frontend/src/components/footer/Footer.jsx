4f529a26f83718cdc9ecd8f3b3d12d7a01239bdd
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-05-19 10:23:31 -0600

    style: modificar el padding del footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer({
   return (
     <footer
       className={`
-        py-8 text-center space-y-1
+        py-4 text-center space-y-1
         ${className}
       `}
     >

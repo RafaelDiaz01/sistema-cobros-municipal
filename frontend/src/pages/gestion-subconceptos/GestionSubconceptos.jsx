395e015cf2f557bd5d5074f593e08d68edada908
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-05-25 09:52:01 -0600

    feat: agregar logica para obtener estadísticas desde el backend

--- frontend/src/pages/gestion-subconceptos/GestionSubconceptos.jsx
+++ frontend/src/pages/gestion-subconceptos/GestionSubconceptos.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, use } from "react";
 import { FileText, Check, X, HandCoins } from "lucide-react";
 import { getSubconceptosAPI } from "../../services/subconceptoService.js";
-import { updateSubconceptoEstadoAPI } from "../../services/subconceptoService.js";
+import { updateSubconceptoEstadoAPI, getEstadisticasSubconceptosAPI } from "../../services/subconceptoService.js";
 import { showToast } from "../../utils/alerts/toast.js";
 import { alertConfirmation } from "../../utils/alerts/alert.js";
 import { subconceptosColumns } from "./subconceptos.columns.jsx";
@@ -11,54 +11,68 @@ import SectionTittle from "../../components/titles/SectionTitle.jsx";
 import StatsCards from "../../components/cards/StatsCards.jsx";
 import Table from "../../components/table/Table.jsx";
 import AddSubconceptoModal from "../../components/features/subconceptos/AddSubconceptosModal.jsx";
+import { set } from "react-hook-form";
 
 export default function Subconceptos() {
   const [subconceptos, setSubconceptos] = useState([]);
+  const [stats, setStats] = useState([]);
+  const [loadingPage, setLoadingPage] = useState(true);
+  const [loadingTable, setLoadingTable] = useState(false);
   const [open, setOpen] = useState(false);
   const [subconceptosEdit, setSubconceptosEdit] = useState(null);
+  const [totalRows, setTotalRows] = useState(0);
+
+  const [paginationModel, setPaginationModel] =
+    useState({
+      page: 0,
+      pageSize: 9,
+    });
+
+  useEffect(() => {
+    cargarModuloInicial();
+  }, []);
 
   useEffect(() => {
     fetchSubconceptos();
-  }, []);
+  }, [paginationModel]);
 
-  const fetchSubconceptos = async () => {
+  const cargarModuloInicial = async () => {
     try {
-      const data = await getSubconceptosAPI();
-      setSubconceptos(data);
+      setLoadingPage(true);
+      await Promise.all([
+        fetchEstadisticas(),
+        fetchSubconceptos(),
+      ]);
     } catch (error) {
-      console.error("Error fetching subconceptos:", error);
+      showToast("error", "Error al cargar datos");
+    } finally {
+      setLoadingPage(false);
     }
   };
 
-  const stats = useMemo(() => {
-    return [
-      {
-        title: "Total Subconceptos",
-        value: subconceptos.length,
-        icon: <FileText size={26} />,
-      },
-      {
-        title: "Subconceptos Activos",
-        value: subconceptos.filter((s) => s.activo).length,
-        icon: <Check size={26} />,
-      },
-      {
-        title: "Subconceptos Inactivos",
-        value: subconceptos.filter((s) => !s.activo).length,
-        icon: <X size={26} />,
-      },
-      {
-        title: "Son Cobrables",
-        value: subconceptos.filter((s) => s.es_cobrable).length,
-        icon: <HandCoins size={26} />,
-      },
-    ];
-  }, [subconceptos]);
+  const fetchEstadisticas = async () => {
+    try {
+      const data = await getEstadisticasSubconceptosAPI();
+      setStats(data);
+    } catch (error) {
+      showToast("error", "Error al cargar estadísticas de subconceptos");
+    }
+  };
 
-  // Abrir modal para crear
-  const handleAdd = () => {
-    setSubconceptosEdit(null);
-    setOpen(true);
+  const fetchSubconceptos = async (cargaInicial = false) => {
+    try {
+      if (!cargaInicial) setLoadingTable(true);
+      const data = await getSubconceptosAPI({
+        page: paginationModel.page + 1,
+        pageSize: paginationModel.pageSize,
+      });
+      setSubconceptos(data.data);
+      setTotalRows(data.total);
+    } catch (error) {
+      showToast("error", "Error al cargar subconceptos");
+    } finally {
+      if (!cargaInicial) setLoadingTable(false);
+    }
   };
 
   const handleDelete = async (id, estadoActual) => {
@@ -76,18 +90,29 @@ export default function Subconceptos() {
 
     try {
       await updateSubconceptoEstadoAPI(id, { estado: nuevoEstado });
-      fetchSubconceptos();
+      await fetchSubconceptos();
+      await fetchEstadisticas();
       showToast("success", "Estado actualizado exitosamente");
     } catch (error) {
-      console.error("Error al cambiar el estado subconcepto", error);
       showToast("error", "Error al cambiar el estado del subconcepto");
     }
   };
 
+  // Abrir modal para crear
+  const handleAdd = () => {
+    setSubconceptosEdit(null);
+    setOpen(true);
+  };
+
+  // Abrir modal para editar
   const handleEdit = (subconcepto) => {
     setSubconceptosEdit(subconcepto);
     setOpen(true);
   };
+
+  const columns = useMemo(() => {
+    return subconceptosColumns(handleEdit, handleDelete);
+  }, []);
 
   return (
     <PageLayout>
@@ -105,11 +130,36 @@ export default function Subconceptos() {
             onSuccess={fetchSubconceptos}
           />
         )}
-        <StatsCards stats={stats} />
+        <StatsCards stats={[
+          {
+            title: "Total Subconceptos",
+            value: stats.total,
+            icon: <FileText size={26} />,
+          },
+          {
+            title: "Subconceptos Activos",
+            value: stats.activos,
+            icon: <Check size={26} />,
+          },
+          {
+            title: "Subconceptos Inactivos",
+            value: stats.inactivos,
+            icon: <X size={26} />,
+          },
+          {
+            title: "Son Cobrables",
+            value: stats.cobrables,
+            icon: <HandCoins size={26} />,
+          },
+        ]} />
         <Table
           rows={subconceptos}
-          columns={subconceptosColumns(handleEdit, handleDelete)}
+          loading={loadingTable}
+          columns={columns}
           getRowId={(row) => row.id_subconcepto}
+          rowCount={totalRows}
+          paginationModel={paginationModel}
+          onPaginationModelChange={setPaginationModel}
         />
       </Stack>
     </PageLayout>

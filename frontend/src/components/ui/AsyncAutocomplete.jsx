042dd75dba90cf654268f735a01911a8a91d67d3
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-05-25 07:51:28 -0600

    feat: agregar helper para mostrar errores en AsyncAutocomplete

--- frontend/src/components/ui/AsyncAutocomplete.jsx
+++ frontend/src/components/ui/AsyncAutocomplete.jsx
@@ -1,6 +1,7 @@
 import { Autocomplete, TextField, CircularProgress } from "@mui/material";
 import { useState, useEffect } from "react";
 import { useDebounce } from "../../hooks/useDebounce.js";
+import Stack from "../../components/layouts/Stack.jsx";
 
 export default function AsyncAutocomplete({
   value,
@@ -12,6 +13,7 @@ export default function AsyncAutocomplete({
   getOptionLabel,
   renderOption,
   placeholder = "",
+  error
 }) {
   const [options, setOptions] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -38,60 +40,68 @@ export default function AsyncAutocomplete({
   }, [debouncedSearch, searchFn]);
 
   return (
-    <Autocomplete
-      disabled={disabled}
-      options={options}
-      loading={loading}
-      getOptionLabel={getOptionLabel}
-      value={value}
-      inputValue={inputValue}
-      onChange={(_, newValue) => onSelect(newValue)}
-      onInputChange={(_, newInputValue) => onInputChange(newInputValue)}
-      filterOptions={(x) => x}
-      renderOption={renderOption}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          placeholder={placeholder}
-          variant="outlined"
-          InputProps={{
-            ...params.InputProps,
-            className:
-              "w-full pl-10 pr-3 py-2 rounded-lg bg-[#F9FAFB] " +
-              "border border-[#E5E7EB] text-sm focus:outline-none",
-            endAdornment: (
-              <>
-                {loading && <CircularProgress size={20} />}
-                {params.InputProps.endAdornment}
-              </>
-            ),
-          }}
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              borderRadius: "0.5rem",
-              backgroundColor: "#F9FAFB",
-              fontSize: "0.875rem",
-              padding: 0,
-              minHeight: "40px",
-              border: "1px solid #E5E7EB",
-              "&.Mui-focused": {
-                borderColor: "var(--color-acento)",
-                boxShadow: "0 0 0 1px var(--color-acento)",
+    <Stack size="xs">
+      <Autocomplete
+        disabled={disabled}
+        options={options}
+        loading={loading}
+        getOptionLabel={getOptionLabel}
+        value={value}
+        inputValue={inputValue}
+        onChange={(_, newValue) => onSelect(newValue)}
+        onInputChange={(_, newInputValue) => onInputChange(newInputValue)}
+        filterOptions={(x) => x}
+        renderOption={renderOption}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            placeholder={placeholder}
+            variant="outlined"
+            InputProps={{
+              ...params.InputProps,
+              className:
+                "w-full pl-10 pr-3 py-2 rounded-lg bg-[#F9FAFB] " +
+                "border border-[#E5E7EB] text-sm focus:outline-none",
+              endAdornment: (
+                <>
+                  {loading && <CircularProgress size={20} />}
+                  {params.InputProps.endAdornment}
+                </>
+              ),
+            }}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                borderRadius: "0.5rem",
+                backgroundColor: "#F9FAFB",
+                fontSize: "0.875rem",
+                padding: 0,
+                minHeight: "40px",
+                border: "1px solid #E5E7EB",
+                "&.Mui-focused": {
+                  borderColor: "var(--color-acento)",
+                  boxShadow: "0 0 0 1px var(--color-acento)",
+                },
               },
-            },
-            "& .MuiOutlinedInput-input": {
-              padding: "8px 12px",
-            },
-            "& .MuiOutlinedInput-notchedOutline": {
-              border: "none",
-            },
-            "& input::placeholder": {
-              color: "#9CA3AF",
-              opacity: 1,
-            },
-          }}
-        />
+              "& .MuiOutlinedInput-input": {
+                padding: "8px 12px",
+              },
+              "& .MuiOutlinedInput-notchedOutline": {
+                border: "none",
+              },
+              "& input::placeholder": {
+                color: "#9CA3AF",
+                opacity: 1,
+              },
+            }}
+          />
+        )}
+      />
+      {/* HELPER */}
+      {error && (
+        <p className="text-red-500 text-xs">
+          {error?.message}
+        </p>
       )}
-    />
+    </Stack>
   );
 }

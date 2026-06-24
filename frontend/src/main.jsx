22b16bf901ad0f2111b587c05853c148c775f9e8
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-06-23 21:28:35 -0600

    feat: Integrar React Query para la obtención de datos

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,9 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import { AuthProvider } from "./context/authContext.jsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "./lib/queryClient.js";
 import theme from "./theme/theme";
 import "../src/assets/styles/global.css";
 
@@ -12,7 +15,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <AuthProvider>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools />
+          </QueryClientProvider>
         </AuthProvider>
       </BrowserRouter>
     </ThemeProvider>
